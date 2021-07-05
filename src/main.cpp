0a0dac0fbb298a7560c07aaf6187d7155e6ac476
Michal Majeran <marjano89@gmail.com>
2021-07-06 00:11:59 +0200

    Added init quard for SDL_Init

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include "SDL2/SDL.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-  SDL_Init(SDL_INIT_VIDEO);
+  if(SDL_Init(SDL_INIT_VIDEO) > 0)
+  {
+    std::cout << "FAILED TO INITIALIZE SDL. SDL_ERROR: " << SDL_GetError() <<std::endl;
+    return -1;
+  }
 
   SDL_Window *window = SDL_CreateWindow(
     "SDL2Test",
